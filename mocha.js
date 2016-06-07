fb851f02cebea49ac11a462f975df860fe9f8175
Denis Davydkov <denis@davydkov.com>
2016-06-07 21:44:47 +0200

    Added latest mocha rules

--- mocha.js
+++ mocha.js
@@ -3,6 +3,11 @@ module.exports = {
         'mocha'
     ],
     'rules': {
-        'mocha/no-exclusive-tests': 2
+        'mocha/no-exclusive-tests': 2,
+        'mocha/no-pending-tests': 2,
+        'mocha/no-skipped-tests': 1,
+        'mocha/handle-done-callback': 2,
+        'mocha/no-synchronous-tests': 0,
+        'mocha/no-global-tests': 0
     }
 };
